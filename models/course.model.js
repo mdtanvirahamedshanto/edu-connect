d329e763e7dcedff47b1aa7c619a69fc0b5a247c
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2024-07-02 22:15:11 +0600

    courses model

--- models/course.model.js
+++ models/course.model.js
@@ -1,7 +1,47 @@
 import mongoose, { Schema } from "mongoose";
 
 const courseSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    thumbnail:{
+        type: String,
+        required: true
+    },
+    modules:{
+        type: [Schema.ObjectId],
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true
+    },
+    category:{
+        type: Schema.ObjectId,
+        required: false
+    },
+    instructor:{
+        type: Schema.ObjectId,
+        required: false
+    },
+    testimonials:{
+        type:[Schema.ObjectId],
+        required: false
+    },
+    quizSet:{
+        type: Schema.ObjectId,
+        required: false
+    }
 
 });
 
-export const Course = mongoose.model.Courses ?? mongoose.model("Courses",courseSchema);
+export const Course = mongoose.model.Course ?? mongoose.model("Course",courseSchema);
